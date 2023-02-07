ea36062027c093f13a541d6872de62bb1f711048
Theo Lomeu Braga <theolomeu@gmail.com>
2023-02-07 16:42:50 -0300

    a

--- tech_demos/test_MV/resources/Scripts/TMP_libs/short_cuts/create_render_shader.lua
+++ tech_demos/test_MV/resources/Scripts/TMP_libs/short_cuts/create_render_shader.lua
@@ -2,5 +2,15 @@ require("TMP_libs.objects.game_object")
 require("TMP_libs.components.transform")
 require("TMP_libs.components.render_shader")
 
-function create_render_shader()
+function create_render_shader(father,is_ui,pos,rot,sca,mat)
+    ret = game_object:new(father)
+    ret:add_component(components.transform)
+    ret.components[components.transform].is_ui = is_ui
+    ret.components[components.transform].position = deepcopy(pos)
+    ret.components[components.transform].rotation = deepcopy(rot)
+    ret.components[components.transform].scale = deepcopy(sca)
+    ret.components[components.transform]:set()
+
+    
+    return ret
 end
